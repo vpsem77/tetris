010deef1cb8b36da861d4b717c1d210f76cbfd93
vpsem77 <97038784+vpsem77@users.noreply.github.com>
2026-01-14 16:23:19 +0900

    Update tetris.c

--- tetris.c
+++ tetris.c
@@ -263,8 +263,6 @@ char menu(){
 	return wgetch(stdscr);
 }
 
-/////////////////////////첫주차 실습에서 구현해야 할 함수/////////////////////////
-
 int CheckToMove(char f[HEIGHT][WIDTH],int currentBlock,int blockRotate, int blockY, int blockX){
 	// user code
 	for(int i = 0; i < 4; i++){
@@ -278,11 +276,7 @@ int CheckToMove(char f[HEIGHT][WIDTH],int currentBlock,int blockRotate, int bloc
 }
 
 void DrawChange(char f[HEIGHT][WIDTH],int command,int currentBlock,int blockRotate, int blockY, int blockX){
-	// user code
-
-	//1. 이전 블록 정보를 찾는다. ProcessCommand의 switch문을 참조할 것
-	//2. 이전 블록 정보를 지운다. DrawBlock함수 참조할 것.
-	//3. 새로운 블록 정보를 그린다. 
+	
 	int nx = blockX, ny = blockY, nr = blockRotate;
 	switch(command){
 		case KEY_UP:
@@ -347,8 +341,6 @@ void BlockDown(int sig){
 			DrawField();
 		}
 	}
-	// blockY++; move(0, 0); printw(blockY);
-	//강의자료 p26-27의 플로우차트를 참고한다.
 }
 
 int AddBlockToField(char f[HEIGHT][WIDTH],int currentBlock,int blockRotate, int blockY, int blockX){
@@ -363,14 +355,9 @@ int AddBlockToField(char f[HEIGHT][WIDTH],int currentBlock,int blockRotate, int 
 		}
 	}
 	return touched * 10;
-	//Block이 추가된 영역의 필드값을 바꾼다.
 }
 
 int DeleteLine(char f[HEIGHT][WIDTH]){
-	// user code
-
-	//1. 필드를 탐색하여, 꽉 찬 구간이 있는지 탐색한다.
-	//2. 꽉 찬 구간이 있으면 해당 구간을 지운다. 즉, 해당 구간으로 필드값을 한칸씩 내린다.
 	int res = 0;
 	for(int i = 0; i < HEIGHT; i++){
 		int cnt = 0;
@@ -410,24 +397,12 @@ void DrawBlockWithFeatures(int y, int x, int blockID, int blockRotate){
 }
 
 void createRankList(){
-	// 목적: Input파일인 "rank.txt"에서 랭킹 정보를 읽어들임, 읽어들인 정보로 랭킹 목록 생성
-	// 1. "rank.txt"열기
-	// 2. 파일에서 랭킹정보 읽어오기
-	// 3. LinkedList로 저장
-	// 4. 파일 닫기
+	
 	FILE *fp;
 	int i, j, score;
 	char name[NAMELEN + 1];
-	//1. 파일 열기
 	fp = fopen("rank.txt", "r");
-
-	// 2. 정보읽어오기
-	/* int fscanf(FILE* stream, const char* format, ...);
-	stream:데이터를 읽어올 스트림의 FILE 객체를 가리키는 파일포인터
-	format: 형식지정자 등등
-	변수의 주소: 포인터
-	return: 성공할 경우, fscanf 함수는 읽어들인 데이터의 수를 리턴, 실패하면 EOF리턴 */
-	// EOF(End Of File): 실제로 이 값은 -1을 나타냄, EOF가 나타날때까지 입력받아오는 if문
+	
 	head = malloc(sizeof(NODE));
 	head->next = NULL;
 	if (fscanf(fp, "%d", &score_number) != EOF) {
@@ -446,26 +421,19 @@ void createRankList(){
 	else {
 		score_number = 0;
 	}
-	// 4. 파일닫기
 	fclose(fp);
 }
 
 void rank(){
-	//목적: rank 메뉴를 출력하고 점수 순으로 X부터~Y까지 출력함
-	//1. 문자열 초기화
 	int X=1, Y=score_number, ch, i, j;
 	clear();
 
-	//2. printw()로 3개의 메뉴출력
 	printw("1. list ranks from X to Y\n");
 	printw("2. list ranks by a specific name\n");
 	printw("3. delete a specific rank\n");
 
-	//3. wgetch()를 사용하여 변수 ch에 입력받은 메뉴번호 저장
 	ch = wgetch(stdscr);
 
-	//4. 각 메뉴에 따라 입력받을 값을 변수에 저장
-	//4-1. 메뉴1: X, Y를 입력받고 적절한 input인지 확인 후(X<=Y), X와 Y사이의 rank 출력
 	if (ch == '1') {
 		echo();
 		printw("X: ");
@@ -490,7 +458,6 @@ void rank(){
 		noecho();
 	}
 
-	//4-2. 메뉴2: 문자열을 받아 저장된 이름과 비교하고 이름에 해당하는 리스트를 출력
 	else if ( ch == '2') {
 		char str[NAMELEN+1];
 		int check = 0;
@@ -520,7 +487,6 @@ void rank(){
 		noecho();
 	}
 
-	//4-3. 메뉴3: rank번호를 입력받아 리스트에서 삭제
 	else if ( ch == '3') {
 		int num;
 		echo();
@@ -548,41 +514,27 @@ void rank(){
 }
 
 void writeRankFile(){
-	// 목적: 추가된 랭킹 정보가 있으면 새로운 정보를 "rank.txt"에 쓰고 없으면 종료
 	int sn, i;
-	//1. "rank.txt" 연다
 	FILE *fp = fopen("rank.txt", "w");
 
-	//2. 랭킹 정보들의 수를 "rank.txt"에 기록
 	fprintf(fp, "%d\n", score_number);
-	//3. 탐색할 노드가 더 있는지 체크하고 있으면 다음 노드로 이동, 없으면 종료
-	NODE *node = head->next;  // head는 dummy 노드라고 가정
+	NODE *node = head->next;  
     while (node) {
         fprintf(fp, "%s %d\n", node->user, node->score);
         node = node->next;
     }
-	// if ( sn == score_number) return;
-	// else {
-	// }
-	// for ( i= 1; i < score_number+1 ; i++) {
-	// 	free(a.rank_name[i]);
-	// }
-	// free(a.rank_name);
-	// free(a.rank_score);
+
 	fclose(fp);
 }
 
 void newRank(int score){
-	// 목적: GameOver시 호출되어 사용자 이름을 입력받고 score와 함께 리스트의 적절한 위치에 저장
 	char str[NAMELEN+1];
 	int i, j;
 	clear();
-	//1. 사용자 이름을 입력받음	
 	echo();
 	printw("your name: ");
 	scanw("%s", str);
 	noecho();
-	// 2. 새로운 노드를 생성해 이름과 점수를 저장, score_number가
 	NODE* node = malloc(sizeof(NODE));
 	node->score = score;
 	strcpy(node->user, str);
@@ -609,8 +561,7 @@ void DrawRecommend(int y, int x, int blockID,int blockRotate){
 }
 
 int recommend(TNode *root){
-	int max=0; // 미리 보이는 블럭의 추천 배치까지 고려했을 때 얻을 수 있는 최대 점수
-	// user code
+	int max=0; 
 	int y, x, score;
 	root->child = (TNode**) malloc(sizeof(TNode*) * (CHILDREN_MAX + 1)); 
 	root->accumulatedScore = -1000;
@@ -640,7 +591,6 @@ int recommend(TNode *root){
 					score += recommend(root->child[i]);;
 				}else root->child[i]->level = root->level + 1;
 
-				// 점수의 최댓값을 구한다.
 				if(score > root->accumulatedScore || (score == root->accumulatedScore && root->recBlockY < y)) {
 					root->accumulatedScore = score;
 					root->recBlockY = y;
